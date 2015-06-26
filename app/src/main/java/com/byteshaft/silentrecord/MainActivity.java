d788dd165438813e27437d5c378813a60f6a2fcb
Husnain Kareem <husnain446@gmail.com>
2015-06-26 12:32:12 +0500

    tab feature implement

--- app/src/main/java/com/byteshaft/silentrecord/MainActivity.java
+++ app/src/main/java/com/byteshaft/silentrecord/MainActivity.java
@@ -1,8 +1,19 @@
 package com.byteshaft.silentrecord;
 
 import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.Color;
+import android.graphics.drawable.ClipDrawable;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
@@ -11,21 +22,44 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import it.neokree.materialtabs.MaterialTab;
+import it.neokree.materialtabs.MaterialTabHost;
+import it.neokree.materialtabs.MaterialTabListener;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends ActionBarActivity implements MaterialTabListener{
 
     private DrawerLayout mDrawerLayout;
     private ActionBarDrawerToggle mDrawerToggle;
     private String[] mListTitles;
     private ListView mDrawerList;
+    private ViewPager mViewPager;
+    private MaterialTabHost mMaterialTabHost;
+    private Resources mResources;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        android.support.v7.app.ActionBar actionBar = getSupportActionBar();
+        actionBar.setBackgroundDrawable(new ColorDrawable(Color.parseColor("#000000")));
         setContentView(R.layout.activity_main);
+        mMaterialTabHost = (MaterialTabHost) findViewById(R.id.tab_host);
+        mViewPager = (ViewPager) findViewById(R.id.pager);
+        mResources = getResources();
+        ViewPagerAdapter mViewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager());
+        mViewPager.setAdapter(mViewPagerAdapter);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setHomeButtonEnabled(true);
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mViewPager.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener(){
+            @Override
+            public void onPageSelected(int position) {
+                mMaterialTabHost.setSelectedNavigationItem(position);
+            }
+        });
+        for (int i = 0; i < mViewPagerAdapter.getCount(); i++) {
+            mMaterialTabHost.addTab(mMaterialTabHost.newTab().setIcon(getIcon(i)).setTabListener(this));
+        }
         mDrawerToggle = getActionBarDrawerToggle();
         mDrawerLayout.setDrawerListener(mDrawerToggle);
         mDrawerList = (ListView) findViewById(R.id.list_drawer);
@@ -106,6 +140,22 @@ public class MainActivity extends AppCompatActivity {
         };
     }
 
+    @Override
+    public void onTabSelected(MaterialTab materialTab) {
+        mViewPager.setCurrentItem(materialTab.getPosition());
+
+    }
+
+    @Override
+    public void onTabReselected(MaterialTab materialTab) {
+
+    }
+
+    @Override
+    public void onTabUnselected(MaterialTab materialTab) {
+
+    }
+
     private class DrawerItemClickListener implements ListView.OnItemClickListener {
 
         @Override
@@ -114,4 +164,52 @@ public class MainActivity extends AppCompatActivity {
             mDrawerLayout.closeDrawer(mDrawerList);
         }
     }
+
+    private class ViewPagerAdapter extends FragmentStatePagerAdapter {
+
+        public ViewPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        public Fragment getItem(int num) {
+            Fragment result = new Fragment();
+
+            switch (num) {
+                case 0:
+                    result = new VideosActivity();
+                    break;
+                case 1:
+                    result = new ImagesActivity();
+                    break;
+                case 2:
+                    result = new ScheduleActivity();
+                    break;
+            }
+            return result;
+        }
+
+        @Override
+        public int getCount() {
+            return 3;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return null;
+        }
+    }
+
+    private Drawable getIcon(int position) {
+        switch (position) {
+            case 0:
+                return mResources.getDrawable(R.drawable.videos);
+            case 1:
+                return mResources.getDrawable(R.drawable.images);
+            case 2:
+                return mResources.getDrawable(R.drawable.schedule);
+            default:
+                return null;
+        }
+    }
+
 }
