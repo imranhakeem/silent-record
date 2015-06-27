86c826442e1b8d1c84bb479258deed64cd9d4e6f
Omer Akram <om26er@gmail.com>
2015-06-27 20:45:39 +0500

    preliminary video and picture taking

---
+++ app/src/main/java/com/byteshaft/silentrecord/CustomCamera.java
@@ -0,0 +1,133 @@
+package com.byteshaft.silentrecord;
+
+import android.content.Context;
+import android.content.ContextWrapper;
+import android.hardware.Camera;
+import android.media.MediaRecorder;
+import android.os.Environment;
+
+import com.byteshaft.ezflashlight.CameraStateChangeListener;
+import com.byteshaft.ezflashlight.Flashlight;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class CustomCamera extends ContextWrapper implements CameraStateChangeListener,
+        Camera.ShutterCallback, Camera.PictureCallback {
+
+    private static CustomCamera sCustomCamera;
+    private Flashlight mFlashlight;
+    private int mCameraRequest;
+    private MediaRecorder mMediaRecorder;
+
+    private static class CameraRequest {
+        static final int START_RECORDING = 1;
+        static final int TAKE_PICTURE = 2;
+    }
+
+    private CustomCamera(Context base) {
+        super(base);
+        mFlashlight = new Flashlight(getApplicationContext());
+        mFlashlight.setCameraStateChangedListener(this);
+    }
+
+    static CustomCamera getInstance(Context context) {
+        if (sCustomCamera == null) {
+            sCustomCamera = new CustomCamera(context);
+        }
+        return sCustomCamera;
+    }
+
+    void takePicture() {
+        mCameraRequest = CameraRequest.TAKE_PICTURE;
+        mFlashlight.setupCameraPreview();
+    }
+
+    private void takePicture(Camera camera) {
+        Camera.Parameters parameters = camera.getParameters();
+        parameters.setRotation(90);
+        camera.setParameters(parameters);
+        camera.setDisplayOrientation(90);
+        camera.startPreview();
+        camera.autoFocus(new Camera.AutoFocusCallback() {
+            @Override
+            public void onAutoFocus(boolean b, Camera camera) {
+                camera.takePicture(CustomCamera.this, null, null, CustomCamera.this);
+            }
+        });
+    }
+
+    void startRecording() {
+        mCameraRequest = CameraRequest.START_RECORDING;
+        mFlashlight.setupCameraPreview();
+    }
+
+    private void startRecording(Camera camera) {
+        String path = Environment.getExternalStorageDirectory() + File.separator + "test.3gp";
+        mMediaRecorder = new MediaRecorder();
+        mMediaRecorder.setCamera(camera);
+        mMediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        mMediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+        mMediaRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+        mMediaRecorder.setOutputFile(path);
+        try {
+            mMediaRecorder.prepare();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        mMediaRecorder.start();
+    }
+
+    void stopRecording() {
+        mMediaRecorder.stop();
+        mMediaRecorder.reset();
+        mMediaRecorder.release();
+        mMediaRecorder = null;
+    }
+
+    @Override
+    public void onCameraInitialized() {
+
+    }
+
+    @Override
+    public void onCameraViewSetup(Camera camera) {
+        switch (mCameraRequest) {
+            case CameraRequest.TAKE_PICTURE:
+                takePicture(camera);
+                break;
+            case CameraRequest.START_RECORDING:
+                startRecording(camera);
+                break;
+        }
+    }
+
+    @Override
+    public void onCameraBusy() {
+
+    }
+
+    @Override
+    public void onPictureTaken(byte[] bytes, Camera camera) {
+        String out = Environment.getExternalStorageDirectory() + File.separator + "test.jpg";
+        File file = new File(out);
+        try {
+            FileOutputStream outputStream = new FileOutputStream(file);
+            outputStream.write(bytes);
+            outputStream.flush();
+            outputStream.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        mFlashlight.releaseAllResources();
+    }
+
+    @Override
+    public void onShutter() {
+
+    }
+}
--- app/src/main/java/com/byteshaft/silentrecord/ScheduleActivity.java
+++ app/src/main/java/com/byteshaft/silentrecord/ScheduleActivity.java
@@ -7,11 +7,40 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
-public class ScheduleActivity extends Fragment {
+public class ScheduleActivity extends Fragment implements View.OnClickListener {
+
+    private boolean mIsRecording;
+
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.schedule, container, false);
+        View view = inflater.inflate(R.layout.schedule, container, false);
+        Button pictureButton = (Button) view.findViewById(R.id.button_picture);
+        Button videoButton = (Button) view.findViewById(R.id.button_record);
+        pictureButton.setOnClickListener(this);
+        videoButton.setOnClickListener(this);
+        return view;
+    }
+
+    @Override
+    public void onClick(View view) {
+        CustomCamera camera  = CustomCamera.getInstance(getActivity().getApplicationContext());
+        switch (view.getId()) {
+            case R.id.button_picture:
+                camera.takePicture();
+                break;
+            case R.id.button_record:
+                if (mIsRecording) {
+                    camera.stopRecording();
+                    mIsRecording = false;
+                } else {
+                    camera.startRecording();
+                    mIsRecording = true;
+                }
+                break;
+        }
     }
 }
