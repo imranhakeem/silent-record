3ef02091233006fec14c0f0871c8988de51366c7
scorpion01 <shgr8rb@gmail.com>
2015-06-29 18:01:44 +0500

    add resolution list

--- app/src/main/java/com/byteshaft/silentrecord/SettingFragment.java
+++ app/src/main/java/com/byteshaft/silentrecord/SettingFragment.java
@@ -1,8 +1,13 @@
 package com.byteshaft.silentrecord;
 
+import android.hardware.Camera;
 import android.os.Bundle;
+import android.preference.ListPreference;
+import android.preference.Preference;
 
 import com.github.machinarius.preferencefragment.PreferenceFragment;
+
+import java.util.ArrayList;
 
 
 public class SettingFragment extends PreferenceFragment {
@@ -11,5 +16,34 @@ public class SettingFragment extends PreferenceFragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.prefs);
+        Preference test = findPreference("video_resolution");
+        ListPreference lp = (ListPreference) test;
+        lp.setEntries(getBackCameraResolution());
+        lp.setEntryValues(getBackCameraResolution());
+    }
+
+    String[] getBackCameraResolution() {
+        int videoHeight;
+        int videoWidth;
+        ArrayList<String> resolutionList = new ArrayList<>();
+        int noOfCameras = Camera.getNumberOfCameras();
+        for (int i = 0;i < noOfCameras;i++) {
+            Camera.CameraInfo cameraInfo = new Camera.CameraInfo();
+            Camera.getCameraInfo(i, cameraInfo);
+            if (cameraInfo.facing == Camera.CameraInfo.CAMERA_FACING_BACK) {
+                Camera camera = Camera.open(i);
+                Camera.Parameters cameraParams = camera.getParameters();
+                for (Camera.Size size : cameraParams.getSupportedPictureSizes()) {
+                    videoHeight = size.width;
+                    videoWidth  = size.height;
+                    String height = String.valueOf(videoHeight);
+                    String width = String.valueOf(videoWidth);
+                    resolutionList.add(height +" X " +width);
+                }
+                camera.release();
+            }
+        }
+        String names[]=resolutionList.toArray(new String[resolutionList.size()]);
+        return  names;
     }
 }
