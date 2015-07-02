c89065b78a7d81b31502676f77e87bf1462f35a3
bilal shahid <s9iper1@gmail.com>
2015-07-02 13:17:28 +0500

    schedule inprogress

--- app/src/main/java/com/byteshaft/silentrecord/Helpers.java
+++ app/src/main/java/com/byteshaft/silentrecord/Helpers.java
@@ -1,17 +1,34 @@
 package com.byteshaft.silentrecord;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.ContextWrapper;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
+import android.text.format.Time;
+import android.util.Log;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 
 public class Helpers extends ContextWrapper {
 
     SharedPreferences mSharedPreferences;
+    private AlarmManager mAlarmManager;
+    private PendingIntent mPIntent;
+    private boolean mTimeSet;
+    private boolean mDateSet;
 
     public Helpers(Context base) {
         super(base);
+    }
+
+    private AlarmManager getAlarmManager() {
+        return (AlarmManager) getSystemService(Context.ALARM_SERVICE);
     }
 
     String readZoomSettings() {
@@ -19,4 +36,67 @@ public class Helpers extends ContextWrapper {
         String zoomControl = mSharedPreferences.getString("camera_zoom_control", "20");
         return zoomControl;
     }
+
+    void setAlarmForVideoRecording(int date, int month,
+                                   int year, int hour, int minutes) {
+        mAlarmManager = getAlarmManager();
+        Intent intent = new Intent("com.byteShaft.videoRecordingAlarm");
+        mPIntent = PendingIntent.getBroadcast(getApplicationContext(), 1, intent, 0);
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.DATE,date);  //1-31
+        calendar.set(Calendar.MONTH, month);  //first month is 0!!! January is zero!!!
+        calendar.set(Calendar.YEAR,year);//year...
+
+        calendar.set(Calendar.HOUR_OF_DAY, hour);  //HOUR
+        calendar.set(Calendar.MINUTE, minutes);       //MIN
+        mAlarmManager.setInexactRepeating(AlarmManager.RTC_WAKEUP,
+                calendar.getTimeInMillis(), AlarmManager.INTERVAL_DAY, mPIntent);
+        Log.i("Video_Alarm,", "setting alarm of :" + calendar.getTime());
+    }
+
+    private SharedPreferences getPreferenceManager() {
+       return PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+    }
+
+    void picAlarmset(boolean picAlarm) {
+        SharedPreferences sharedPreferences = getPreferenceManager();
+        sharedPreferences.edit().putBoolean("picAlarm", picAlarm).commit();
+    }
+
+    boolean getPicAlarmStatus() {
+        SharedPreferences sharedPreferences = getPreferenceManager();
+        return sharedPreferences.getBoolean("picAlarm", false);
+    }
+
+    void videoAlarmset(boolean picAlarm) {
+        SharedPreferences sharedPreferences = getPreferenceManager();
+        sharedPreferences.edit().putBoolean("picAlarm", picAlarm).commit();
+    }
+
+    boolean getVideoAlarmStatus() {
+        SharedPreferences sharedPreferences = getPreferenceManager();
+        return sharedPreferences.getBoolean("picAlarm", false);
+    }
+
+    void setTime(boolean value) {
+        SharedPreferences sharedPreferences = getPreferenceManager();
+        sharedPreferences.edit().putBoolean("time_set", value).commit();
+    }
+
+    boolean getTime() {
+        SharedPreferences sharedPreferences = getPreferenceManager();
+        return sharedPreferences.getBoolean("time_set", false);
+    }
+
+    void setDate(boolean value) {
+        SharedPreferences sharedPreferences = getPreferenceManager();
+        sharedPreferences.edit().putBoolean("date_time", value).commit();
+    }
+
+    boolean getDate() {
+        SharedPreferences sharedPreferences = getPreferenceManager();
+        return sharedPreferences.getBoolean("date_time", false);
+    }
+
+
 }
--- app/src/main/java/com/byteshaft/silentrecord/ScheduleActivity.java
+++ app/src/main/java/com/byteshaft/silentrecord/ScheduleActivity.java
@@ -1,6 +1,7 @@
 package com.byteshaft.silentrecord;
 
 
+
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -8,26 +9,87 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Toast;
 
-import com.byteshaft.ezflashlight.Flashlight;
+import com.fourmob.datetimepicker.date.DatePickerDialog;
+import com.sleepbot.datetimepicker.time.RadialPickerLayout;
+import com.sleepbot.datetimepicker.time.TimePickerDialog;
 import com.byteshaft.ezflashlight.FlashlightGlobals;
 
-public class ScheduleActivity extends Fragment implements View.OnClickListener {
+import java.util.Calendar;
+
+public class ScheduleActivity extends Fragment implements View.OnClickListener,
+        DatePickerDialog.OnDateSetListener, TimePickerDialog.OnTimeSetListener{
 
     private boolean mIsRecording;
     private Button mVideoButton;
     private Button mPictureButton;
+    private Button mBtnTimePicker;
+    private Button mBtnDatePicker;
+    private DatePickerDialog datePickerDialog;
+    private TimePickerDialog timePickerDialog;
+    static final String DATEPICKER_TAG = "datepicker";
+    static final String TIMEPICKER_TAG = "timepicker";
+    private Helpers mHelpers;
+    private Button mPicButton;
+    private Button mVideoBtn;
 
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.schedule, container, false);
+        mHelpers = new Helpers(getActivity());
         mPictureButton = (Button) view.findViewById(R.id.button_picture);
         mVideoButton = (Button) view.findViewById(R.id.button_record);
+        mBtnDatePicker = (Button) view.findViewById(R.id.btnSetDate);
+        mBtnTimePicker = (Button) view.findViewById(R.id.btnSetTime);
+        mPicButton = (Button) view.findViewById(R.id.buttonPic);
+        mVideoBtn = (Button) view.findViewById(R.id.buttonVideo);
+        setBackgroundForButtonPresent();
+        final Calendar calendar = Calendar.getInstance();
+        datePickerDialog = DatePickerDialog.newInstance(this, calendar.get(Calendar.YEAR),
+                calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH));
+        timePickerDialog = TimePickerDialog.newInstance(this, calendar.get(Calendar.HOUR_OF_DAY)
+                , calendar.get(Calendar.MINUTE), false, false);
+        if (savedInstanceState != null) {
+            DatePickerDialog dpd = (DatePickerDialog)
+                    getFragmentManager().findFragmentByTag(DATEPICKER_TAG);
+            if (dpd != null) {
+                dpd.setOnDateSetListener(this);
+            }
+
+            TimePickerDialog tpd = (TimePickerDialog)
+                    getFragmentManager().findFragmentByTag(TIMEPICKER_TAG);
+            if (tpd != null) {
+                tpd.setOnTimeSetListener(this);
+            }
+        }
+        mBtnDatePicker.setOnClickListener(this);
+        mBtnTimePicker.setOnClickListener(this);
         mPictureButton.setOnClickListener(this);
         mVideoButton.setOnClickListener(this);
+        mPicButton.setOnClickListener(this);
+        mVideoBtn.setOnClickListener(this);
         return view;
+    }
+
+    private void setBackgroundForButtonPresent() {
+        if (mHelpers.getDate()) {
+            mBtnDatePicker.setBackgroundResource(R.drawable.date_set);
+        } else {
+            mBtnDatePicker.setBackgroundResource(R.drawable.date);
+        }
+        if (mHelpers.getTime()) {
+            mBtnTimePicker.setBackgroundResource(R.drawable.time_set);
+        } else {
+            mBtnTimePicker.setBackgroundResource(R.drawable.time);
+        }
+        if (mHelpers.getTime() && mHelpers.getDate()) {
+            mPicButton.setBackgroundResource(R.drawable.pic_set);
+        } else {
+            mPicButton.setBackgroundResource(R.drawable.pic);
+        }
     }
 
     @Override
@@ -50,6 +112,34 @@ public class ScheduleActivity extends Fragment implements View.OnClickListener {
                     mIsRecording = true;
                 }
                 break;
+            case R.id.btnSetDate:
+                datePickerDialog.setYearRange(2015, 2037);
+                datePickerDialog.show(getFragmentManager(), DATEPICKER_TAG);
+                break;
+            case R.id.btnSetTime:
+                timePickerDialog.show(getFragmentManager(), TIMEPICKER_TAG);
+                break;
+            case R.id.buttonPic:
+
+                break;
+            case R.id.buttonVideo:
+                break;
         }
+    }
+
+    @Override
+    public void onDateSet(DatePickerDialog datePickerDialog, int year, int month, int day) {
+        Toast.makeText(getActivity(),"year: "+ year + "Month :"+ month +"Day :" +day,Toast.LENGTH_SHORT).show();
+        mBtnDatePicker.setBackgroundResource(R.drawable.date_set);
+        mHelpers.setDate(true);
+    }
+
+    @Override
+    public void onTimeSet(RadialPickerLayout radialPickerLayout, int hours, int minutes) {
+        Toast.makeText(getActivity(),"Hours: "+ hours + "Minutes :"+ minutes,Toast.LENGTH_SHORT).show();
+        mBtnTimePicker.setBackgroundResource(R.drawable.time_set);
+        mHelpers.setTime(true);
+
+
     }
 }
