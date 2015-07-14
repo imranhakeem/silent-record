b4b1cbc89ca1d479af4f20ad1b484b5fb3c1c166
Omer Akram <om26er@gmail.com>
2015-07-14 18:00:47 +0500

    Select video and image to report bug

--- app/src/main/java/com/byteshaft/silentrecord/fragments/ReportFragment.java
+++ app/src/main/java/com/byteshaft/silentrecord/fragments/ReportFragment.java
@@ -1,6 +1,11 @@
 package com.byteshaft.silentrecord.fragments;
 
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -12,6 +17,8 @@ import android.widget.Toast;
 
 import com.byteshaft.silentrecord.AppGlobals;
 import com.byteshaft.silentrecord.R;
+
+import java.io.File;
 
 public class ReportFragment extends Fragment {
 
@@ -45,7 +52,41 @@ public class ReportFragment extends Fragment {
                 }
             }
         });
+        Button attachButton = (Button) reportView.findViewById(R.id.button_attach_report);
+        attachButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(Intent.ACTION_PICK);
+                i.setType("video/*, images/*");
+                startActivityForResult(i, 0);
+            }
+        });
         return reportView;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 0 && data != null) {
+            Uri contactUri = data.getData();
+            String file = getRealPathFromURI(getActivity(), contactUri);
+            Toast.makeText(getActivity(), "Selected " + new File(file).getName(), Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public String getRealPathFromURI(Context context, Uri contentUri) {
+        Cursor cursor = null;
+        try {
+            String[] proj = { MediaStore.Images.Media.DATA };
+            cursor = context.getContentResolver().query(contentUri,  proj, null, null, null);
+            int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+            cursor.moveToFirst();
+            return cursor.getString(column_index);
+        } finally {
+            if (cursor != null) {
+                cursor.close();
+            }
+        }
     }
 
     @Override
